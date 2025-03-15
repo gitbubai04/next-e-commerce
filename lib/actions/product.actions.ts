d01c051a14c57d43c88f2d406027c895b20a5128
bubai <bubaiapp444@gmail.com>
2025-03-16 04:14:59 +0530

    16/03 04:14 am

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,7 +1,7 @@
 "use service";
-import { PrismaClient } from "@prisma/client";
 import { objectToJS } from "@/lib/utils";
 import { LATEST_PRODUCTS_COUNT } from "../constants";
+import { PrismaClient } from "@prisma/client";
 
 // get latest products
 export async function getLatestProducts() {
@@ -10,6 +10,17 @@ export async function getLatestProducts() {
   const data = await prisma.product.findMany({
     orderBy: { createdAt: "desc" },
     take: LATEST_PRODUCTS_COUNT,
+  });
+
+  return objectToJS(data);
+}
+
+//get single prodict by its slug
+export async function getProductBySlug(slug: string) {
+  const prisma = new PrismaClient();
+
+  const data = await prisma.product.findFirst({
+    where: { slug },
   });
 
   return objectToJS(data);
